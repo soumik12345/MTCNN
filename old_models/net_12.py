a224f9effe0c92b554c95f2a0cede899f4e79b8d
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2019-10-22 17:56:31 +0530

    Added 12net callibration

--- old_models/net_12.py
+++ old_models/net_12.py
@@ -24,3 +24,24 @@ class Net12:
 
     def summarize(self):
         self.model.summary()
+
+
+
+class Net12Calibration:
+
+    def __init__(self, input_shape = (12, 12, 3)):
+        self.input_shape = input_shape
+    
+
+    def build_network(self):
+        input_layer = Input(shape = self.input_shape)
+        x = Conv2D(16, (3, 3), strides = 1, padding = 'same')(input_layer)
+        x = MaxPool2D(pool_size = 3, strides = 2)(x)
+        x = Flatten()(x)
+        x = Dense(128)(x)
+        x = Dense(2, activation = 'softmax')(x)
+        self.model = Model(input_layer, x)
+    
+
+    def summarize(self):
+        self.model.summary()
